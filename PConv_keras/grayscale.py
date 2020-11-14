195b2dba051e590158684b31ea7dad68814752b1
dnwjddl <woojung980305@gmail.com>
2020-11-15 02:44:20 +0900

    Update grayscale.py

--- PConv_keras/grayscale.py
+++ PConv_keras/grayscale.py
@@ -1,16 +1,16 @@
 import cv2
 import numpy as np
-class model:
-    proto = 'models/colorization_deploy_v2.prototxt'
-    weights = 'models/colorization_release_v2.caffemodel'
 
-    net = cv2.dnn.readNetFromCaffe(proto, weights)
+proto = 'models/colorization_deploy_v2.prototxt'
+weights = 'models/colorization_release_v2.caffemodel'
 
-    pts_in_hull = np.load('models/pts_in_hull.npy')
-    pts_in_hull = pts_in_hull.transpose().reshape(2, 313, 1, 1).astype(np.float32)
-    net.getLayer(net.getLayerId('class8_ab')).blobs = [pts_in_hull]
+net = cv2.dnn.readNetFromCaffe(proto, weights)
 
-    net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full((1, 313), 2.606, np.float32)]
+pts_in_hull = np.load('models/pts_in_hull.npy')
+pts_in_hull = pts_in_hull.transpose().reshape(2, 313, 1, 1).astype(np.float32)
+net.getLayer(net.getLayerId('class8_ab')).blobs = [pts_in_hull]
+
+net.getLayer(net.getLayerId('conv8_313_rh')).blobs = [np.full((1, 313), 2.606, np.float32)]
 
 
 net.setInput(blob)#딥러닝 모델에 삽입
